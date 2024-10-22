82f558a9caa9ff6ebd848b11c6f73839c08838f6
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-10-22 18:39:24 +0700

    Update price formatting in PlaceCard component

--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -1,64 +1,75 @@
 import React from "react";
 import PlaceCard from "./PlaceCard";
-import Img1 from "../../assets/places/boat.jpg";
-import Img2 from "../../assets/places/tajmahal.jpg";
-import Img3 from "../../assets/places/water.jpg";
-import Img4 from "../../assets/places/place4.jpg";
-import Img5 from "../../assets/places/place5.jpg";
-import Img6 from "../../assets/places/place6.jpg";
+import Img1 from "../../assets/places/pantai-kuta.webp";
+import Img2 from "../../assets/places/candi-borobudur.jpeg";
+import Img3 from "../../assets/places/raja-ampat.jpeg";
+import Img4 from "../../assets/places/danau-toba.jpg";
+import Img5 from "../../assets/places/gunung-bromo.jpg";
+import Img6 from "../../assets/places/pulau-komodo.webp";
+
+const formatRupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    currencyDisplay: "symbol",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
 
 const PlacesData = [
   {
     img: Img1,
-    title: "Boat",
-    location: "USA",
-    description: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    price: 6700,
-    type: "Cultural Relax",
+    title: "Pantai Kuta",
+    location: "Bali, Indonesia",
+    description:
+      "Pantai Kuta terkenal dengan pasir putih dan ombak yang cocok untuk berselancar, serta menjadi tempat favorit untuk menikmati matahari terbenam.",
+    price: 2000000,
+    type: "Pantai & Alam",
   },
   {
     img: Img2,
-    title: "Taj Mahal",
-    location: "India",
+    title: "Candi Borobudur",
+    location: "Magelang, Jawa Tengah",
     description:
-      "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the river Yamuna in the Indian city of Agra.",
-    price: 6700,
-    type: "Cultural Relax",
+      "Candi Borobudur adalah candi Buddha terbesar di dunia dan menjadi salah satu Situs Warisan Dunia UNESCO.",
+    price: 1500000,
+    type: "Budaya & Sejarah",
   },
   {
     img: Img3,
-    title: "Underwater",
-    location: "US",
+    title: "Raja Ampat",
+    location: "Papua Barat, Indonesia",
     description:
-      "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the river Yamuna in the Indian city of Agra.",
-    price: 6200,
-    type: "Cultural Relax",
+      "Raja Ampat terkenal dengan keindahan bawah lautnya yang memukau, surga bagi penyelam dan pencinta snorkeling.",
+    price: 5000000,
+    type: "Pantai & Alam",
   },
   {
     img: Img4,
-    title: "Sydney",
-    location: "USA",
-    description: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    price: 6700,
-    type: "Cultural Relax",
+    title: "Danau Toba",
+    location: "Sumatera Utara, Indonesia",
+    description:
+      "Danau Toba adalah danau vulkanik terbesar di dunia dan menawarkan pemandangan yang indah serta budaya Batak yang unik.",
+    price: 2500000,
+    type: "Danau & Pegunungan",
   },
   {
     img: Img5,
-    title: "Los Angeles",
-    location: "United states",
+    title: "Gunung Bromo",
+    location: "Jawa Timur, Indonesia",
     description:
-      "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the river Yamuna in the Indian city of Agra.",
-    price: 6700,
-    type: "Cultural Relax",
+      "Gunung Bromo menawarkan pengalaman sunrise yang spektakuler dan merupakan destinasi favorit bagi wisatawan lokal dan mancanegara.",
+    price: 1800000,
+    type: "Gunung & Petualangan",
   },
   {
     img: Img6,
-    title: "Los Vegas",
-    location: "California",
+    title: "Pulau Komodo",
+    location: "Nusa Tenggara Timur, Indonesia",
     description:
-      "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the river Yamuna in the Indian city of Agra.",
-    price: 6200,
-    type: "Cultural Relax",
+      "Pulau Komodo adalah rumah bagi hewan langka Komodo dan menawarkan keindahan pantai serta perairan jernih yang memukau.",
+    price: 3000000,
+    type: "Petualangan & Alam",
   },
 ];
 
@@ -67,8 +78,8 @@ const Places = ({ handleOrderPopup }) => {
     <>
       <div className="dark:bg-gray-900 dark:text-white bg-gray-50 py-10">
         <section data-aos="fade-up" className="container ">
-          <h1 className=" my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-bold">
-            Best Places to Visit
+          <h1 className="my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-bold">
+            Tempat Wisata Terbaik di Indonesia
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {PlacesData.map((item, index) => (
@@ -76,6 +87,7 @@ const Places = ({ handleOrderPopup }) => {
                 handleOrderPopup={handleOrderPopup}
                 key={index}
                 {...item}
+                price={formatRupiah(item.price)}
               />
             ))}
           </div>
