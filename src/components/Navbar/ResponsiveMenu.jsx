a582e83ce3dde90c28fd41749b8ba6a22b3588ef
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-10-22 23:56:59 +0700

    Refactor ResponsiveMenu component: Update language to Bahasa Indonesia and update footer text

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -8,14 +8,14 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
     <div
       className={`${
         showMenu ? "left-0" : "-left-[100%]"
-      } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 lg:hidden rounded-r-xl shadow-md`} // Ganti md dengan lg
+      } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 lg:hidden rounded-r-xl shadow-md`}
     >
       <div className="card">
         <div className="flex items-center justify-start gap-3">
           <FaUserCircle size={50} />
           <div>
-            <h1>Hello User</h1>
-            <h1 className="text-sm text-slate-500">Premium user</h1>
+            <h1>Halo Pengguna</h1>
+            <h1 className="text-sm text-slate-500">Pengguna Premium</h1>
           </div>
         </div>
         <nav className="mt-12">
@@ -35,9 +35,7 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
         </nav>
       </div>
       <div className="footer">
-        <h1>
-          Made with ❤ by <a href="https://dilshad-ahmed.github.io/">Dilshad</a>{" "}
-        </h1>
+        <h1 className="text-sm text-center">&copy; 2024 All rights reserved</h1>
       </div>
     </div>
   );
